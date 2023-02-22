be411feb19c8a8207e5c7b911b169a74a1311c79
Tirth24052002 <tirth.p@simformsolutions.com>
2023-02-22 18:13:20 +0530

    -Initial-Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var greeting = "Hello, playground"
+import UIKit
+
+var greeting = "Hello, playground"
+//print variables of different types and print it.
+let b = 10
+var a = 5
+print("a")
+print("b")
